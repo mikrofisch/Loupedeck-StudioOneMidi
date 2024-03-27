fa4b307b8e2ab08995d149b3d008a406e78aa4ca
krawumski <jens@rumbergdesign.de>
2024-03-27 10:31:25 +0100

    Modes buttons field - work in progress.

--- src/StudioOneMidiPlugin/Controls/LoupedeckModes.cs
+++ src/StudioOneMidiPlugin/Controls/LoupedeckModes.cs
@@ -5,6 +5,7 @@
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+    using System.Windows.Navigation;
     using System.Xml.Linq;
 
     // This is a special button class that creates 6 buttons which are then
@@ -12,24 +13,82 @@
     // The functionality of the buttons changes dynamically.
     internal class LoupedeckModes : PluginDynamicCommand
     {
-        private enum buttonLayer
+        // common
+        StudioOneMidiPlugin plugin;
+        private enum ButtonLayer
         {
             channelProperties,
             faderModesAll,
             faderModesSend
         }
 
-        private IDictionary<string, string> buttonData = new Dictionary<string, string>();
+        private IDictionary<string, ModeButtonData> buttonData = new Dictionary<string, ModeButtonData>();
+
+        private abstract class ModeButtonData
+        {
+            public abstract BitmapImage getImage(PluginImageSize imageSize);
+        }
+
+        ButtonLayer currentLayer = ButtonLayer.channelProperties;
+
+        private class PropertyButtonData : ModeButtonData
+        {
+            public ChannelProperty.BoolType Type;
+            public BitmapImage Icon = null;
+
+            public PropertyButtonData(ChannelProperty.BoolType bt, string iconName = null)
+            {
+                Type = bt;
+                if (iconName != null)
+                    Icon = EmbeddedResources.ReadImage(EmbeddedResources.FindFile($"{iconName}_52px.png"));
+            }
+
+            public override BitmapImage getImage(PluginImageSize imageSize)
+            {
+                BitmapBuilder bb = new BitmapBuilder(imageSize);
+
+                if (this.Icon != null)
+                {
+                    bb.DrawImage(this.Icon, bb.Width / 2 - this.Icon.Width / 2, bb.Height);
+                }
+                else
+                {
+                    bb.DrawText(ChannelProperty.boolPropertyLetter[(int)this.Type], 0, 0, bb.Width, bb.Height, null, 32);
+                }
+
+                return bb.ToImage();
+            }
+        }
+
 
         public LoupedeckModes()
         {
             this.Description = "Special button for controlling Loupedeck fader modes";
 
+            // Create UI buttons
             for (int i = 0; i < 6; i++)
             {
                 AddParameter($"{i}", "Mode Group Button " + i, "Modes");
-                buttonData[$"{i}"] = "Button " + i;
             }
+
+            // Create button data for each layer
+            addButton(ButtonLayer.channelProperties, 0, new PropertyButtonData(ChannelProperty.BoolType.Mute));
+            addButton(ButtonLayer.channelProperties, 1, new PropertyButtonData(ChannelProperty.BoolType.Solo));
+            addButton(ButtonLayer.channelProperties, 2, new PropertyButtonData(ChannelProperty.BoolType.Arm, "record"));
+            addButton(ButtonLayer.channelProperties, 3, new PropertyButtonData(ChannelProperty.BoolType.Monitor, "monitor"));
+
+        }
+
+        // common
+        protected override bool OnLoad()
+        {
+            plugin = base.Plugin as StudioOneMidiPlugin;
+
+            plugin.MackieDataChanged += (object sender, EventArgs a) => {
+                ActionImageChanged();
+            };
+
+            return true;
         }
 
         protected override BitmapImage GetCommandImage(string actionParameter, PluginImageSize imageSize)
@@ -37,21 +96,31 @@
             if (actionParameter == null)
                 return null;
 
-            var bb = new BitmapBuilder(imageSize);
+            string idx = $"{(int)currentLayer}:" + actionParameter;
 
-            bb.DrawText(buttonData[actionParameter], 0, 0, bb.Width, bb.Height);
-
-            return bb.ToImage();
+            return buttonData[idx].getImage(imageSize);
         }
 
         protected override void RunCommand(string actionParameter)
         {
-            if (actionParameter == "4")
+            switch (currentLayer)
             {
-                buttonData["0"] = "Wumm";
-                ActionImageChanged();
+                case ButtonLayer.channelProperties:
+                    break;
+                case ButtonLayer.faderModesAll:
+                    break;
+                case ButtonLayer.faderModesSend:
+                    break;
             }
         }
+
+        private void addButton(ButtonLayer buttonLayer, int buttonIndex, ModeButtonData bd)
+        {
+            string idx = $"{(int)buttonLayer}:{buttonIndex}";
+            buttonData[idx] = bd;
+        }
     }
+
 }
+
 

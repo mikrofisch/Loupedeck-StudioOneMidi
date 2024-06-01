cf4b239df4243571bb143baa51fe6188ffeb3369
krawumski <jens@rumbergdesign.de>
2024-06-01 18:27:05 +0200

    Color management for user pages working (edit dialog for pot values only so far).

--- src/StudioOneMidiPlugin/UserPotConfig.xaml.cs
+++ src/StudioOneMidiPlugin/UserPotConfig.xaml.cs
@@ -11,6 +11,7 @@
     using System.Windows.Controls;
     using System.Windows.Data;
     using System.Windows.Documents;
+    using System.Windows.Forms.VisualStyles;
     using System.Windows.Input;
     using System.Windows.Media;
     using System.Windows.Media.Imaging;
@@ -46,16 +47,16 @@
     public partial class UserPotConfig : Window
     {
         private UserPotConfigData ConfigData;
-
+        private ColorFinder UserColorFinder;
 
         private Plugin Plugin { get; set; }
-        public UserPotConfig(Plugin plugin, UserPotConfigData configData)
+        public UserPotConfig(Plugin plugin, ColorFinder cf, UserPotConfigData configData)
         {
             this.Plugin = plugin;
+            this.UserColorFinder = cf;
 
             this.InitializeComponent();
 
-//            this.ConfigData = new UserPotConfigData(configData);
             this.ConfigData = configData;
 
             this.DataContext = this.ConfigData;
@@ -99,13 +100,37 @@
         private void SetPluginSetting(String valueID, String value) => 
             this.Plugin.SetPluginSetting(ColorFinder.settingName(this.ConfigData.PluginName,
                                                                  this.ConfigData.PluginParameter,
-                                                                 valueID), value);
+                                                                 valueID), value, true);
+        private void Reset(Object sender, RoutedEventArgs e)
+        {
+            var settingsList = this.Plugin.ListPluginSettings();
+
+            foreach (var setting in settingsList)
+            {
+                if (setting.StartsWith(ColorFinder.settingName(this.ConfigData.PluginName,
+                                                                    this.ConfigData.PluginParameter, "")))
+                {
+                    this.Plugin.DeletePluginSetting(setting);
+                }
+            }
+            this.UserColorFinder.Init(this.Plugin, forceReload: true);
+
+            var onColor = this.UserColorFinder.getOnColor(this.ConfigData.PluginName, this.ConfigData.PluginParameter);
+
+            this.ConfigData.Mode = this.UserColorFinder.getMode(this.ConfigData.PluginName, this.ConfigData.PluginParameter);
+            this.rbPositive.IsChecked = this.ConfigData.Mode == ColorFinder.ColorSettings.PotMode.Positive;
+            this.rbSymmetric.IsChecked = this.ConfigData.Mode == ColorFinder.ColorSettings.PotMode.Symmetric;
+            this.tbColorR.Text = onColor.R.ToString();
+            this.tbColorG.Text = onColor.G.ToString();
+            this.tbColorB.Text = onColor.B.ToString();
+            this.tbLabel.Text = this.UserColorFinder.getLabel(this.ConfigData.PluginName, this.ConfigData.PluginParameter);
+        }
         private void SaveAndClose(Object sender, RoutedEventArgs e)
         {
-            var textOnColorHex = ((Byte)this.tbColorR.Text.ParseInt32()).ToString("X2") +
-                                 ((Byte)this.tbColorG.Text.ParseInt32()).ToString("X2") +
-                                 ((Byte)this.tbColorB.Text.ParseInt32()).ToString("X2");
-            this.SetPluginSetting(ColorFinder.ColorSettings.strTextOnColor, textOnColorHex);
+            var onColorHex = ((Byte)this.tbColorR.Text.ParseInt32()).ToString("X2") +
+                             ((Byte)this.tbColorG.Text.ParseInt32()).ToString("X2") +
+                             ((Byte)this.tbColorB.Text.ParseInt32()).ToString("X2");
+            this.SetPluginSetting(ColorFinder.ColorSettings.strOnColor, onColorHex);
             this.SetPluginSetting(ColorFinder.ColorSettings.strLabel, this.tbLabel.Text);
             this.SetPluginSetting(ColorFinder.ColorSettings.strMode, $"{(this.rbPositive.IsChecked == true ? 0 : 1)}");
             this.Close();

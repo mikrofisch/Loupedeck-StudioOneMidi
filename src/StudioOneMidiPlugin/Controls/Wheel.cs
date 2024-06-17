3f8c9b443200e053245acbf4cbc0c06b5b2bbe9a
krawumski <jens@rumbergdesign.de>
2024-06-18 00:23:40 +0200

    Adding support for moving the cursor via a MIDI controller message triggering S1's 'freecursortime' function.

---
+++ src/StudioOneMidiPlugin/Controls/Wheel.cs
@@ -0,0 +1,56 @@
+﻿namespace Loupedeck.StudioOneMidiPlugin.Controls
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Melanchall.DryWetMidi.Common;
+
+    using Melanchall.DryWetMidi.Core;
+
+    // This class implements control of the wheel function in Studio One based on the functionality
+    // of a Mackie MCU device.
+
+    public class Wheel : PluginDynamicAdjustment
+    {
+        // This variable holds the current value of the counter.
+        private Int32 Counter = 0;
+
+        // Initializes the adjustment class.
+        // When `hasReset` is set to true, a reset command is automatically created for this adjustment.
+        public Wheel()
+            : base(displayName: "Wheel", description: "Counts rotation ticks", groupName: "Control", hasReset: false)
+        {
+        }
+
+
+
+        // This method is called when the adjustment is executed.
+        protected override void ApplyAdjustment(String actionParameter, Int32 diff)
+        {
+            if (diff < 0)
+            {
+                diff = 128 + diff;
+                if (diff < 64) diff = 64;
+            }
+            if (diff > 127) diff = 127;
+            var e = new ControlChangeEvent();
+            e.ControlValue = (SevenBitNumber)diff;
+            e.ControlNumber = (SevenBitNumber)0x3C;
+            (this.Plugin as StudioOneMidiPlugin).mackieMidiOut.SendEvent(e);
+
+            this.AdjustmentValueChanged();
+        }
+
+        // This method is called when the reset command related to the adjustment is executed.
+        protected override void RunCommand(String actionParameter)
+        {
+        }
+
+        // Returns the adjustment value that is shown next to the dial.
+        protected override String GetAdjustmentValue(String actionParameter) => this.Counter.ToString();
+    }
+    
+
+}
